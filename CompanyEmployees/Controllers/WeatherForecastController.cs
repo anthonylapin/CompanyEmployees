eb1b29c02aa6b87b5f9717accf471d9219d860ef
Anton2 Lapin <a2.lapin@itransition.com>
2020-12-16 13:25:33 +0300

    Add project files.

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private ILoggerManager _logger;
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message froom our values controller");
+            _logger.LogDebug("Here is debug message froom our values controller");
+            _logger.LogWarn("Here is warning message froom our values controller");
+            _logger.LogError("Here is error message froom our values controller");
+
+            return new string[] { "hello", "world" };
+        }
+    }
+}
