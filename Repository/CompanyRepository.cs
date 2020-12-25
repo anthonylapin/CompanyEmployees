be61c833e23618ad10b8e5ab7464df82e27ac238
Anton2 Lapin <a2.lapin@itransition.com>
2020-12-25 16:36:23 +0300

    DeleteCompany action added

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -35,5 +35,10 @@ namespace Repository
         public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
             FindByCondition(c => ids.Contains(c.Id), trackChanges)
             .ToList();
+
+        public void DeleteCompany(Company company)
+        {
+            Delete(company);
+        }
     }
 }
